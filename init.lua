bee6d098e466215d0e17abd54841ad9209e7b59b
Tom <45657686+seidobllik@users.noreply.github.com>
2025-04-01 21:42:04 -0700

    Removed all files not in use.

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
 --[[ Core Configs ]]--
 require("core.options")
 require("core.keymaps")
---require("core.treesitter")
---require("core.lsp")
 require("core.statusline")
---require("core.snippet")
 
 --[[ Packages ]]--
 require("packages.nvim-tree")
